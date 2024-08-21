8bea3ec8fa84b5914058a7cccec01dab3ed6bad5
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2024-08-21 11:53:11 +0200

    Cleanup utils, drop tiktoken

--- instrumentation/opentelemetry-instrumentation-openai/src/opentelemetry/instrumentation/openai/utils.py
+++ instrumentation/opentelemetry-instrumentation-openai/src/opentelemetry/instrumentation/openai/utils.py
@@ -13,35 +13,6 @@
 # limitations under the License.
 
 import logging
-from tiktoken import get_encoding
-
-
-def estimate_tokens_using_tiktoken(prompt, model):
-    """
-    Estimate the number of tokens in a prompt using tiktoken."""
-    encoding = get_encoding(model)
-    tokens = encoding.encode(prompt)
-    return len(tokens)
-
-
-def estimate_tokens(prompt):
-    """
-    Estimate the number of tokens in a prompt."""
-    if prompt and len(prompt) > 0:
-        # Simplified token estimation: count the words.
-        return len([word for word in prompt.split() if word])
-    return 0
-
-
-TIKTOKEN_MODEL_MAPPING = {
-    "gpt-4": "cl100k_base",
-    "gpt-4-32k": "cl100k_base",
-    "gpt-4-0125-preview": "cl100k_base",
-    "gpt-4-1106-preview": "cl100k_base",
-    "gpt-4-1106-vision-preview": "cl100k_base",
-    "gpt-4o": "0200k_base",
-    "gpt-4o-mini": "0200k_base",
-}
 
 
 def silently_fail(func):
@@ -65,20 +36,15 @@ def silently_fail(func):
 
 
 def extract_content(choice):
+    if not hasattr(choice, "message"):
+        return ""
+
     # Check if choice.message exists and has a content attribute
-    if (
-        hasattr(choice, "message")
-        and hasattr(choice.message, "content")
-        and choice.message.content is not None
-    ):
+    if getattr(choice.message, "content", None) is not None:
         return choice.message.content
 
     # Check if choice.message has tool_calls and extract information accordingly
-    elif (
-        hasattr(choice, "message")
-        and hasattr(choice.message, "tool_calls")
-        and choice.message.tool_calls is not None
-    ):
+    if getattr(choice.message, "tool_calls", None) is not None:
         result = [
             {
                 "id": tool_call.id,
@@ -93,27 +59,11 @@ def extract_content(choice):
         return result
 
     # Check if choice.message has a function_call and extract information accordingly
-    elif (
-        hasattr(choice, "message")
-        and hasattr(choice.message, "function_call")
-        and choice.message.function_call is not None
-    ):
+    if getattr(choice.message, "function_call", None) is not None:
         return {
             "name": choice.message.function_call.name,
             "arguments": choice.message.function_call.arguments,
         }
 
     # Return an empty string if none of the above conditions are met
-    else:
-        return ""
-
-
-def calculate_prompt_tokens(prompt_content, model):
-    """
-    Calculate the number of tokens in a prompt. If the model is supported by tiktoken, use it for the estimation.
-    """
-    try:
-        tiktoken_model = TIKTOKEN_MODEL_MAPPING[model]
-        return estimate_tokens_using_tiktoken(prompt_content, tiktoken_model)
-    except Exception:
-        return estimate_tokens(prompt_content)  # Fallback method
+    return ""

273eabefc2e4b9cd6617edc69505528db2f617d0
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2026-03-12 16:22:10 +0100

    Move weaver fixture into conftest

--- tests/weaver-tests/tests/flask/test_flask.py
+++ tests/weaver-tests/tests/flask/test_flask.py
@@ -13,49 +13,6 @@ FLASK_PORT = 5000
 DIR = os.path.join(os.path.dirname(__file__))
 
 
-@pytest.fixture()
-def weaver_binary():
-    # FIXME: don't hardcode weaver bin path
-    weaver_bin = "/home/rm/src/weaver/./target/release/weaver"
-    application_path = os.path.join(os.path.dirname(__file__))
-
-    weaver = subprocess.Popen(
-        [
-            weaver_bin,
-            "registry",
-            "live-check",
-            "--inactivity-timeout=10",  # FIXME: don't hardcode timeout
-            "--format=json",
-            "--no-stats",
-            "--output",
-            DIR,
-        ],
-        cwd=application_path,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-
-    time.sleep(2)
-
-    ready = False
-    for i in range(10):
-        try:
-            response = requests.post("http://localhost:4320", timeout=5)
-        except Exception:
-            continue
-        if response.status_code == 404:
-            ready = True
-            break
-        time.sleep(0.5)
-
-    if not ready:
-        weaver.terminate()
-        weaver.wait(timeout=5)
-        raise Exception("sad trombone")
-
-    yield weaver
-
-
 @pytest.fixture
 def flask_fixture():
     wrapper = shutil.which("opentelemetry-instrument")
@@ -89,6 +46,7 @@ def flask_fixture():
     handler.wait(timeout=5)
 
 
+@pytest.mark.parametrize("weaver_binary", [DIR], indirect=True)
 def test_flask_request(weaver_binary, flask_fixture):
     response = requests.get(f"http://127.0.0.1:{FLASK_PORT}/rolldice")
     assert response.status_code == 200

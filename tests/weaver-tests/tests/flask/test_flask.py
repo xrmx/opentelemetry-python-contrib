6db7b8db8f886d258cff3374c28871a318474e60
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2026-05-11 15:54:41 +0200

    Rework flask tests to use test-utils helper

--- tests/weaver-tests/tests/flask/test_flask.py
+++ tests/weaver-tests/tests/flask/test_flask.py
@@ -1,91 +1,67 @@
-import json
 import os
 import os.path
 import shutil
 import subprocess
 import time
 
-import pytest
 import requests
 
-FLASK_PORT = 5000
-
-DIR = os.path.join(os.path.dirname(__file__))
+from opentelemetry.test.weaver_live_check import WeaverLiveCheck
 
 
-@pytest.fixture
-def flask_fixture():
-    wrapper = shutil.which("opentelemetry-instrument")
-    opentelemetry_instrumentation = [
-        wrapper,
-        # FIXME: looks like there's a race condition somewhere where metrics override the span key in the result :(
-        "--metric_export_interval",
-        "4000",
-        # "--exporter_otlp_endpoint",
-        # otlp_endpoint,
-    ]
+class FlaskApplication:
+    def __init__(self, port=35000):
+        # FIXME: double check that metrics have enough time to be exported
 
-    application_path = os.path.join(os.path.dirname(__file__))
-    handler = subprocess.Popen(
-        opentelemetry_instrumentation
-        + ["flask", "run", "-p", f"{FLASK_PORT}"],
-        cwd=application_path,
-        env={
-            **os.environ,
-            "OTEL_SEMCONV_STABILITY_OPT_IN": "http",  # comment to make tests fail
-        },  # use stable http semconv
-    )
+        self.port = port
+        wrapper = shutil.which("opentelemetry-instrument")
 
-    time.sleep(1)
-
-    yield
-
-    time.sleep(1)
-
-    handler.terminate()
-    handler.wait(timeout=5)
-
-
-@pytest.mark.parametrize("weaver_binary", [DIR], indirect=True)
-def test_flask_request(weaver_binary, flask_fixture):
-    response = requests.get(f"http://127.0.0.1:{FLASK_PORT}/rolldice")
-    assert response.status_code == 200
-
-    response = requests.get(f"http://127.0.0.1:{FLASK_PORT}/")
-    assert response.status_code == 404
-
-    # 5 seconds are needed to avoid grpc errors
-    time.sleep(5)
-
-    # stop weaver
-    requests.post("http://localhost:4320/stop")
-
-    outs, errs = weaver_binary.communicate(timeout=5)
-
-    report_path = os.path.join(DIR, "live_check.json")
-    with open(report_path, "r") as f:
-        report_content = f.read()
-        report = json.loads(report_content)
-
-    assert report
-
-    if "span" in report:
-        span_attributes_violations = [
-            (advice["signal_name"], advice["message"])
-            for attribute in report["span"]["attributes"]
-            for advice in attribute["live_check_result"]["all_advice"]
-            if advice["level"] == "violation"
+        opentelemetry_instrumentation = [
+            wrapper,
         ]
 
-        assert span_attributes_violations == []
-
-    if "metric" in report:
-        metric_attributes_violations = [
-            (advice["signal_name"], advice["message"])
-            for data_point in report["metric"]["data_points"]
-            for attribute in data_point["attributes"]
-            for advice in attribute["live_check_result"]["all_advice"]
-            if advice["level"] == "violation"
+        self.application_path = os.path.join(os.path.dirname(__file__))
+        self.command = opentelemetry_instrumentation + [
+            "flask",
+            "run",
+            "-p",
+            f"{self.port}",
         ]
 
-        assert metric_attributes_violations == []
+    def __enter__(self):
+        self.handler = subprocess.Popen(
+            self.command,
+            cwd=self.application_path,
+            env={
+                **os.environ,
+                "OTEL_SEMCONV_STABILITY_OPT_IN": "http",  # use stable http semconv
+            },
+        )
+
+        time.sleep(1)
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        # FIXME: double check there's enough time for metrics to be exported
+        # 1 seconds are needed to avoid grpc errors
+        time.sleep(1)
+
+        self.handler.terminate()
+        self.handler.wait(timeout=5)
+
+
+def test_flask_request():
+    with WeaverLiveCheck() as weaver:
+        with FlaskApplication() as flask_app:
+            response = requests.get(
+                f"http://127.0.0.1:{flask_app.port}/rolldice"
+            )
+            assert response.status_code == 200
+
+            response = requests.get(f"http://127.0.0.1:{flask_app.port}/")
+            assert response.status_code == 404
+
+        report = weaver.end()
+
+    assert report.violations == []
